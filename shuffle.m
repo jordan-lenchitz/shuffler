c32698f86d2b844300770d5221b27a5126983bc1
jordan-lenchitz <59626477+jordan-lenchitz@users.noreply.github.com>
2022-09-17 13:43:55 -0400

    Update shuffle.m

--- shuffle.m
+++ shuffle.m
@@ -1,12 +1,12 @@
-function shuffle(filename, block_size, file_name)
+function shuffle(sound, block_size, output)
 if nargin == 2
     check = 0;
 else
     check = 1;
 end
 global x1
-nSamples = audioinfo(filename).TotalSamples;
-Fs = audioinfo(filename).SampleRate;
+nSamples = audioinfo(sound).TotalSamples;
+Fs = audioinfo(sound).SampleRate;
 curSample = 1; q=1;
 while (curSample <= nSamples)
     N1 = curSample;
@@ -14,7 +14,7 @@ while (curSample <= nSamples)
     if (N2 > nSamples)
         N2 = nSamples;
     end
-    monoslice(filename, N1, N2)
+    monoslice(sound, N1, N2)
     try
         j(q,:) = x1;
     catch
@@ -30,7 +30,7 @@ for (i=2:size(random_j, 1))
     shuffled = [shuffled;random_j(i,:)'];
 end
 if check > 0
-    audiowrite(file_name, shuffled ./ abs(max(shuffled)), Fs)
+    audiowrite(output, shuffled ./ abs(max(shuffled)), Fs)
 else
     sound(shuffled, Fs)
 end
